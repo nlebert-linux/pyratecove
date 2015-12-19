ff94c6213314b088717ec546b76a73a231ca9512
Nathan LeBert <nlebert.linux@gmail.com>
2015-12-19 15:08:10 -0600

    Production Update

--- pyratecove/settings/production.py
+++ pyratecove/settings/production.py
@@ -2,3 +2,13 @@ from base import *
 
 
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
